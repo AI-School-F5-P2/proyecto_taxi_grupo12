a3f5180b17a3add7221060b94f9026001b00ec5d
taniamvitoria <taniamvitoria@gmail.com>
2023-06-15 12:04:48 +0200

    Adicionar Archivo Taximetro.py

---
+++ venv/Taximetro.py
@@ -0,0 +1,18 @@
+from pynput import keyboard
+
+def pulsa(tecla):
+# Verificar si la tecla presionada es una tecla específica
+    if tecla == keyboard.Key.space:
+        print("Espacio presionado")
+    elif tecla == keyboard.Key.enter:
+        print("Intro presionado")
+    else:
+        print("Esta tecla presionada no tiene fuction!!")
+
+# Crear un Listener para escuchar las pulsaciones de teclas
+with keyboard.Listener(on_press=pulsa) as escuchador:
+    escuchador.join()
+
+
+
+
