a8e6ffebe3bc86996b025dab6f6dedd5c09e7418
saccg <135819811+saccg@users.noreply.github.com>
2023-06-19 12:37:11 +0200

    Definición de variables

--- Taximetro.py
+++ Taximetro.py
@@ -14,6 +14,21 @@ with keyboard.Listener(on_press=pulsa) as escuchador:
     escuchador.join()
 
 
+import time
+from pynput import keyboard
+
+tiempo_inicio = None
+en_movimiento = False
+tiempo_total = 0
+
+def calcular_tarifa(tiempo, en_movimiento):
+    if en_movimiento:
+        tarifa = tiempo * 0.05  # 5 céntimos por segundo
+    else:
+        tarifa = tiempo * 0.02  # 2 céntimos por segundo
+    return tarifa
+
+
 
 
 
