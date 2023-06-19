2ef6e34739ed90dfe128bfa7a11772e92359bbe2
saccg <135819811+saccg@users.noreply.github.com>
2023-06-19 12:20:10 +0200

    Cambios pruebas de código

--- Taximetro.py
+++ Taximetro.py
@@ -1,17 +1,29 @@
-    from pynput import keyboard
+from pynput import keyboard
 
 def pulsa(tecla):
-# Verificar si la tecla presionada es una tecla específica
+ Verificar si la tecla presionada es una tecla específica
     if tecla == keyboard.Key.space:
         print("Espacio presionado")
     elif tecla == keyboard.Key.enter:
-        print("Intro presionado")
+       # print("Intro presionado")
     else:
         print("Esta tecla presionada no tiene fuction!!")
 
-# Crear un Listener para escuchar las pulsaciones de teclas
+ Crear un Listener para escuchar las pulsaciones de teclas
 with keyboard.Listener(on_press=pulsa) as escuchador:
     escuchador.join()
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
